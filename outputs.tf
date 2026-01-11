239cb85b4985522b6247c86d0f47b4331dcb05d5
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:49:00 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "mssql_virtual_network_rules" {
+  description = "All mssql_virtual_network_rule resources"
+  value       = azurerm_mssql_virtual_network_rule.mssql_virtual_network_rules
+}
+
